0ce6d1204786046306e03346521245f9c84b727c
Geonhee Son <ryan.son1002@gmail.com>
2022-06-19 14:45:55 +0900

    Initial Commit

---
+++ ECommnerceSample/ECommnerceSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommnerceSampleApp.swift
+//  ECommnerceSample
+//
+//  Created by Geonhee on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct ECommnerceSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
